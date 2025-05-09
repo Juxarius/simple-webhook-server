06bad5060710ca617eb5602842808a7e545c4427
Juxarius <exdarkrb@gmail.com>
2025-05-09 15:33:59 +0800

    Adding real logic

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, Request, HTTPException
+import hmac
+from fastapi import FastAPI, Request, HTTPException, Header
 import uvicorn
-import json
 import subprocess
-import os
 
 app = FastAPI()
 secrets_file = 'secrets.json'
@@ -11,22 +10,30 @@ def get_secrets() -> dict:
     with open(secrets_file, 'r') as f:
         return f.read()
 
+def is_valid_github_request(body: bytes, x_hub_signature_256: str):
+    h = hmac.new(get_secrets()["githubSecret"].encode(), body, 'sha256')
+    return hmac.compare_digest(h.hexdigest(), x_hub_signature_256)
+
 counter = 1
 
-@app.post("/restart")
-async def restart_service(request: Request):
+@app.post("/restart/github")
+async def restart_service(request: Request, x_hub_signature_256: str=Header(None)):
     global counter
+    body = await request.body()
+    if not is_valid_github_request(body, x_hub_signature_256):
+        raise HTTPException(status_code=403, detail="Forbidden")
+
     data = await request.json()
-    with open(f"received{counter}.txt", 'w+') as f:
-        json.dump(data, f, indent=4)
-        counter += 1
-    # if data.get("password") != get_secrets():
-    #     raise HTTPException(status_code=403, detail="Forbidden")
-    # try:
-    #     subprocess.run(["sudo", "systemctl", "restart", "blitz.service"], check=True)
-    #     return {"status": "restarted"}
-    # except subprocess.CalledProcessError as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+    repo_data = get_secrets()["restart"].get(data["repository"]["full_name"], None)
+    if repo_data is None:
+        raise HTTPException(status_code=404, detail="Repo not found")
+
+    try:
+        await subprocess.run(["sudo", "su", "-", repo_data["user"],"-c", f"cd {repo_data['appDir']} && git pull"])
+        await subprocess.run(["sudo", "systemctl", "restart", repo_data["service"]])
+        return {"status": "restarted"}
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == '__main__':
     uvicorn.run(
